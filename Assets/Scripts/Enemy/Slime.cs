e408fd64175f38826837cb3af3ffcc9228ec9a2f
ClementLSW <clementlsw@gmail.com>
2022-12-14 01:15:38 +0800

    Hehe XD nuke ma branch

---
+++ Assets/Scripts/Enemy/Slime.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slime : Enemy
+{
+    [Header("Components")]
+    public GameObject Player;       // Can probably change datatype back to transform
+    private Rigidbody2D rb;
+    public EnemyStateManager esm;
+
+    void Awake(){
+        Player = GameObject.Find("Player");
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        esm.Update();
+        
+        if(this.HP <= 0){
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col){
+        esm.OnCollision(col);
+    }    
+}

7f5ae62791324eeafa725b608ed20e959c2cb9a1
ClementLSW <clementlsw@gmail.com>
2022-12-15 19:31:19 +0800

    Fixed Slime

--- Assets/Scripts/Enemy/RangedEnemyStateMachine/RangedFireState.cs
+++ Assets/Scripts/Enemy/RangedEnemyStateMachine/RangedFireState.cs
@@ -10,7 +10,8 @@ public class RangedFireState : RangedBaseState
     IEnumerator Fire(Transform pos, Vector3 target){
         yield return new WaitForSeconds(.4f);
         Vector3 dir = (target - pos.position).normalized;
-        Instantiate(enemyProjectile, pos.position, Quaternion.LookRotation(dir));
+        Instantiate(enemyProjectile, pos.position + (dir * 0.2f), Quaternion.LookRotation(dir));
+        Debug.Log("Fire");
         fireComplete = true;
     }
     public override void OnEnter(RangedStateManager rsm){
